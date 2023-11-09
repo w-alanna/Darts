400a6b6cd3d944a21ca571742fc2c47d45b0d2ed
w-alanna <hartalanna2@gmail.com>
2023-11-08 19:31:00 -0500

    added variables and loop to make the points

--- darts.R
+++ darts.R
@@ -4,4 +4,11 @@
 
 
 # Number of darts to throw; the more we throw, the more accurate our estimate
-darts <- 20
+dart_num <- 1000
+raduis <- 0.5
+points <- matrix(nrow=dart_num, ncol=2)
+
+for(x in 1:dart_num) {
+  points[x, 1] <- runif(1, min=0, max=(2*r))
+  points[x, 2] <- runif(1, min=0, max=(2*r))
+}
