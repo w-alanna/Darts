09ae6fb1cd9ba04bf3ddceb7dc207e842a8dfff4
w-alanna <hartalanna2@gmail.com>
2023-11-10 09:51:45 -0500

    fixed conflicts

--- darts.R
+++ darts.R
@@ -1,12 +1,11 @@
 # Monte Carlo darts simulation to compute PI
 # Alanna Hart, Melat Ghebreselassie, November 2023
 
-
-
 # Number of darts to throw; the more we throw, the more accurate our estimate
 dart_num <- 1000
-raduis <- 0.5
-points <- matrix(nrow=dart_num, ncol=2)
+r <- 0.5
+points <- matrix(nrow = dart_num, ncol = 2)
+
 
 for(x in 1:dart_num) {
   points[x, 1] <- runif(1, min=0, max=(2*r))
