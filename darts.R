7f5127ece36e5d11a183e5ec38b065b999bbe73c
w-alanna <hartalanna2@gmail.com>
2023-11-14 21:44:56 -0500

    changed points from matrix to data frame

--- darts.R
+++ darts.R
@@ -3,11 +3,11 @@
 
 # Number of darts to throw; the more we throw, the more accurate our estimate
 dart_num <- 1000
-r <- 0.5
-points <- matrix(nrow = dart_num, ncol = 2)
+raduis <- 0.5
 
+points <- data.frame(x = rep(NA, dart_num), y = rep(NA, dart_num))
 
 for(x in 1:dart_num) {
-  points[x, 1] <- runif(1, min=0, max=(2*r))
-  points[x, 2] <- runif(1, min=0, max=(2*r))
+  points[x, 1] <- runif(1, min=0, max=(2*raduis))
+  points[x, 2] <- runif(1, min=0, max=(2*raduis))
 }
